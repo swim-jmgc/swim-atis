5a814b4569c2a41fd23bcbb9fba97d679e9d769f
swim-jmgc <ma-sasaki@jmgc.co.jp>
2026-06-24 17:44:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,8 @@ def get_atis(icao):
 
     return f"[{icao} ATIS]\n\n{atis}"
     
-    def get_notam(icao):
-        return f"[{icao} NOTAM]\n\nNOTAM取得機能テスト中"
+def get_notam(icao):
+    return f"[{icao} NOTAM]\n\nNOTAM取得機能テスト中"
 
 @app.route("/callback", methods=["POST"])
 
